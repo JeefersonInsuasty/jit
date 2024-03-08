98e5090878df5a8e9aec918858e6f495c1bac3b7
JIT <jefersoninsuasty@gmail.com>
2024-03-07 21:03:06 -0500

    cambio

---
+++ python.py
@@ -0,0 +1,6 @@
+print("Hola a todos") #Funcion print es para imprimir
+
+respuesta = "Pereira" #Esto es una variable que esta guardando un valor 
+respuesta2 = input ("¿Cuál es tu nombre? ")  #Estamos creando un variable dinamica, en la cual se quedara la informacion que la persona coloque en la respuesta  
+print("hola", respuesta2 , "Y bienvenida a casa") # Estamos concatenando un texto con el valor que la persona responde
+
